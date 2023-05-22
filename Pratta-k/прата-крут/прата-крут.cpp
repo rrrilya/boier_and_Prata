c1b8501979fdb145ff8d7ed73355ea1b1fd972c6
rrrilya <130274524+rrrilya@users.noreply.github.com>
2023-05-22 17:40:50 +0500

    Add files via upload

---
+++ Pratta-k/прата-крут/прата-крут.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int search(char* string, char* substring)
+{
+    int  sl, ssl;
+    int res = 0;
+    sl = strlen(string);
+    ssl = strlen(substring);
+    if (sl == 0)
+        cout << "Неверно задана строка\n";
+    else if (ssl == 0)
+        cout << "Неверно задана подстрока\n";
+
+    else
+    {
+        int  i, j = 0, k = -1;
+        int* d = new int[1000];
+        d[0] = -1;
+
+        while (j < ssl - 1)
+        {
+            while (k >= 0 && substring[j] != substring[k])
+                k = d[k];
+            j++;
+            k++;
+            if (substring[j] == substring[k])
+                d[j] = d[k];
+            else
+                d[j] = k;
+        }
+
+        i = 0;
+        j = 0;
+        while (j < ssl && i < sl)
+        {
+            while (j >= 0 && string[i] != substring[j])
+                j = d[j];
+            i++;
+            j++;
+        }
+        delete[] d;
+        res = j == ssl ? i - ssl : 0;
+    }
+    return res;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    char str[256] = "fhbgsv dbg shd hd";
+    char substr[256] = "a;";
+    int pos = search(str, substr);
+
+    if (pos == -1)
+        cout << "Подхождение не найдено!" << "\n";
+    else
+        cout << "Смещение: " << pos << "\n";
+
+    return 0;
+}
---
+++ boier/боера0мура/боера0мура.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+using namespace std;
+
+int search(char* string, char* substring)
+{
+    int  sl = 0;
+    int ssl = 0;
+    int res = -1;
+
+    while (string[sl] != '\0')
+        sl++;
+    while (substring[ssl] != '\0')
+        ssl++;
+
+    if (sl == 0)
+        cout << "Некорректная строка!\n";
+    else if (ssl == 0 or ssl == 1)
+        cout << "Некорректная подстрока!\n";
+    else
+    {
+        int  i, Pos;
+        int  BMT[256];
+        for (i = 0; i < 256; i++)
+            BMT[i] = ssl;
+
+        for (i = ssl - 1; i >= 0; i--)
+            if (BMT[(short)(substring[i])] == ssl)
+                BMT[(short)(substring[i])] = ssl - i - 1;
+
+        Pos = ssl - 1;
+        while (Pos < sl)
+            if (substring[ssl - 1] != string[Pos])
+                Pos = Pos + BMT[(short)(string[Pos])];
+            else
+                for (i = ssl - 2; i >= 0; i--)
+                {
+                    if (substring[i] != string[Pos - ssl + i + 1])
+                    {
+                        Pos += BMT[(short)(string[Pos - ssl + i + 1])] - 1;
+                        break;
+                    }
+                    else if (i == 0)
+                        return Pos - ssl + 1;
+                }
+    }
+    return res;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    char str[256] = "gdjksehjl; ag;lkag ;lnjaghl agwkeu4 agkhga";
+    char substr[256] = "agwk";
+    int pos = search(str, substr);
+
+    if (pos == -1)
+        cout << "Подхождение не найдено!" << "\n";
+    else
+        cout << "Смещение: " << pos << "\n";
+
+    return 0;
+}
